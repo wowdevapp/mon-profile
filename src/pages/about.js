b4bedf58c5690fffb2082ed0c1845a64554b8eef
abdelhaq fouad <m.eng.abdelhaq@gmail.com>
2020-06-17 22:24:43 +0200

    working on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 
 export default class About extends Component {
   render() {
-    return <div>hello from about</div>;
+    return (
+      <div className="about-me">
+        <div className=" container">
+          <div className="title">
+            <h2>about me</h2>
+            <span>ABOUT</span>
+          </div>
+          <div className="row py-4">
+            <div className="col-md-6">title</div>
+            <div className="col-md-6">details</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -7,8 +7,29 @@ export default class Home extends Component {
       <div className="home">
         <div className="home-content text-center">
           <h1 className="name">
-            HI,I'M <span> ABDELHAQ FOUAD</span>
+            hi,i'am <span>abdelhaq fouad</span>
           </h1>
+          <p>
+            I AM A FREELANCER GRAPHIC DESIGNER WEB DEVOLOPER FROM MORROCO I, I
+            HAD STARTED MY CAREER AS A FREELANCER BACK IN 2015.
+          </p>
+          <ul className="sociale">
+            <li>
+              <i className="fa fa-facebook" aria-hidden="true"></i>
+            </li>
+            <li>
+              <i className="fa fa-twitter" aria-hidden="true"></i>
+            </li>
+            <li>
+              <a
+                href="https://github.com/wowdevapp"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <i className="fa fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
         </div>
         <Particles
           params={{
