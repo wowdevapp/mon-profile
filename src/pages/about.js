ab86b8e6b8dfc21c17d18e068289f7a8e6ebfca8
abdelhaq fouad <m.eng.abdelhaq@gmail.com>
2020-06-18 14:43:05 +0200

    finish about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,40 @@ export default class About extends Component {
             <h2>about me</h2>
             <span>ABOUT</span>
           </div>
-          <div className="row py-4">
-            <div className="col-md-6">title</div>
-            <div className="col-md-6">details</div>
+          <div className="row py-5">
+            <div className="col-md-6 my-image">
+              <img className="img-fluid" src="/img/about-image.jpg" alt="" />
+            </div>
+            <div className="col-md-6">
+              <div className="details-content">
+                <h2>
+                  I am <span>Fouad Abdelhaq</span>
+                </h2>
+                <p>
+                  I am a frontend web developer. I can provide clean code and
+                  pixel perfect design. I also make website more & more
+                  interactive with web animations.
+                </p>
+                <ul className="identification">
+                  <li>
+                    <b>Full Name</b> Devid Chester
+                  </li>
+                  <li>
+                    <b>Age</b> 26
+                  </li>
+                  <li>
+                    <b>Nationality </b>marocain
+                  </li>
+                  <li>
+                    <b> Languages</b> french,English,Arabic
+                  </li>
+                  <li>
+                    <b>Address </b>idrissia,hay almowadafine tanger
+                  </li>
+                </ul>
+                <button className="cv btn">DOWNLOAD CV</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
